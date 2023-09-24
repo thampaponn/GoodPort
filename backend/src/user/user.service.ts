899c0e7e4fc8a8b5391f9544ef2b8524493d3940
Madara415 <88467996+madara1150@users.noreply.github.com>
2023-09-24 23:52:30 +0700

    refactor code

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { User } from "./user.model"; // แก้ไขตรงนี้ให้สอดคล้อง
+import { User } from "./user.model";
 
 @Injectable()
 export class UserService {
