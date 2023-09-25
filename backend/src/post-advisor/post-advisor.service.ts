c5be7ca5659f7aec737c5d18f496d3f6b176d93a
Madara415 <88467996+madara1150@users.noreply.github.com>
2023-09-25 11:16:17 +0700

    add

--- backend/src/post-advisor/post-advisor.service.ts
+++ backend/src/post-advisor/post-advisor.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PostAdvisor } from './post-advisor.model';
 
 @Injectable()
-export class PostAdvisorService {}
+export class PostAdvisorService {
+    constructor(@InjectModel("PostAdvisor") private readonly userModel: Model<PostAdvisor>) {}
+
+
+}
