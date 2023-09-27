020c0c78223cfe885f7aa8bcef45333c20a955e4
Madara415 <88467996+madara1150@users.noreply.github.com>
2023-09-27 10:38:14 +0700

    edit page

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { ButtonUi } from "../components/ui/Button";
 import { InputForm } from "../components/ui/InputForm";
-
+// sadasd
 const SignIn = () => {
   return (
     <View style={styles.screen}>
