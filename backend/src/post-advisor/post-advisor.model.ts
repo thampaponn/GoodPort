c07a3046477de48b901333c7cdc1c2475119e81a
Madara415 <88467996+madara1150@users.noreply.github.com>
2023-09-28 00:06:21 +0700

    backend postAdvisor

--- backend/src/post-advisor/post-advisor.model.ts
+++ backend/src/post-advisor/post-advisor.model.ts
@@ -1,13 +1,12 @@
 import * as mongoose from 'mongoose';
 
 export const PostAdvisorSchema = new mongoose.Schema({
-  id: { type: String, required: true },
   detail: { type: String, required: true },
   image: [String],
   createAt: { type: Date, default: Date.now },
   editAt: { type: Date, default: Date.now },
   owner: {
-    professorId: { type: String, required: true },
+    userId: { type: String, required: true },
     fname: { type: String, required: true },
     lname: { type: String, required: true },
     email: { type: String, required: true },
@@ -21,7 +20,7 @@ export interface PostAdvisor extends mongoose.Document {
   createAt: Date;
   editAt: Date;
   owner: {
-    professorId: string;
+    userId: string;
     fname: string;
     lname: string;
     email: string;
