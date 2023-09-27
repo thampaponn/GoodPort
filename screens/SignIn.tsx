c770e4476741a318df9b6582ea7f70d04cb9f705
Madara415 <88467996+madara1150@users.noreply.github.com>
2023-09-27 10:47:07 +0700

    fix: ลบออก

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { ButtonUi } from "../components/ui/Button";
 import { InputForm } from "../components/ui/InputForm";
-// sadasdddd
+
 const SignIn = () => {
   return (
     <View style={styles.screen}>
