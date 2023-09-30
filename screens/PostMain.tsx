c995daa26480d83240250e973fd93f21ae5b2c8b
Nutaya <88426203+Nutaya@users.noreply.github.com>
2023-09-30 15:25:39 +0700

    updatePostMain

--- screens/PostMain.tsx
+++ screens/PostMain.tsx
@@ -8,24 +8,33 @@ const PostMain = () => {
     <ScrollView>
     <View style={styles.screen}>
         <Image source={require('../assets/myProfile.png')} style={styles.img}/>
-        <Text style={{marginTop: 15, fontSize: 16, color: "#0098DA"}}>ความสำเร็จอยู่ที่ไหน</Text>
-        <Text style={{fontSize: 16, color: "#0098DA"}}>ความพยายามอยู่ที่นั่น</Text>
-        <Text style={{marginTop: 10, fontSize: 13, marginRight: 150}}>ประเภทของโครงงาน : </Text>
+        <Text style={{marginTop: 15, fontSize: 16, color: "#0098DA", fontWeight: "bold"}}>ความสำเร็จอยู่ที่ไหน</Text>
+        <Text style={{fontSize: 16, color: "#0098DA", fontWeight: "bold"}}>ความพยายามอยู่ที่นั่น</Text>
+        <Text style={{marginTop: 10, fontSize: 13, marginRight: 140, fontWeight: "bold"}}>ประเภทของโครงงาน : </Text>
         <TouchableOpacity style={styles.button}>
              <Text style={{color: "#FFFFFF", textAlign: "center", fontSize: 16,}}>Edit</Text>
         </TouchableOpacity>
-        <Text style={{marginTop: 15, fontSize: 16, marginRight: 160}}>ชื่อภาษาอังกฤษ : </Text>
-        <Text style={{marginTop: 15, fontSize: 16, marginRight: 195}}>วัตถุประสงค์</Text>
-        <TextInput style={styles.textInput}/>
-        <Text style={{marginTop: 10, fontSize: 16, marginRight: 160}}>ที่มาของโครงงาน</Text>
-        <TextInput style={styles.textInput}/>
-        <Text style={{marginTop: 10, fontSize: 16, marginRight: 140}}>รายละเอียดโครงงาน</Text>
-        <TextInput style={styles.textInput}/>
-        <Text style={{marginTop: 10, fontSize: 16, marginRight: 185}}>รูปที่เกี่ยวข้อง</Text>
-        <Text style={{marginTop: 10, fontSize: 16, marginRight: 175}}>ไฟล์ที่เกี่ยวข้อง</Text>
-        <TextInput style={{width: 280, height: 45, borderColor: "#AEAEAE", borderWidth: 1, borderRadius: 8, marginTop: 5,}}/>
-        <Text style={{marginTop: 10, fontSize: 16, marginRight: 165}}>อาจารย์ที่ปรึกษา</Text>
-        <TextInput style={{width: 280, height: 45, borderColor: "#AEAEAE", borderWidth: 1, borderRadius: 8, marginTop: 5,}}/>
+        <Text style={{marginTop: 15, fontSize: 16, marginRight: 145, fontWeight: "bold"}}>ชื่อภาษาอังกฤษ : </Text>
+        <Text style={{marginTop: 15, fontSize: 16, marginRight: 185, fontWeight: "bold"}}>วัตถุประสงค์</Text>
+        <View style={styles.views}>
+            <Text>eieieieieieieiejcjdsklclksdvcieiei</Text>
+        </View>
+        <Text style={{marginTop: 10, fontSize: 16, marginRight: 150, fontWeight: "bold"}}>ที่มาของโครงงาน</Text>
+        <View style={styles.views}>
+            <Text>eieieieieieieisdnckvksdnkvskvksnvkdsnvdscjfsfnkerfgjkerngkeieiei</Text>
+        </View>
+        <Text style={{marginTop: 10, fontSize: 16, marginRight: 130, fontWeight: "bold"}}>รายละเอียดโครงงาน</Text>
+        <View style={styles.views}>
+            <Text>eieieieieieieieieiei</Text>
+        </View>
+        <Text style={{marginTop: 10, fontSize: 16, marginRight: 180, fontWeight: "bold"}}>รูปที่เกี่ยวข้อง</Text>
+        <Image source={require("../assets/exOne.jpg")} style={styles.expic}/>
+        <Image source={require("../assets/exTwo.jpg")} style={styles.expic}/>
+        <Image source={require("../assets/exThree.jpg")} style={styles.expic}/>
+        <Text style={{marginTop: 10, fontSize: 16, marginRight: 170, fontWeight: "bold"}}>ไฟล์ที่เกี่ยวข้อง</Text>
+        <View style={styles.views2}></View>
+        <Text style={{marginTop: 10, fontSize: 16, marginRight: 160, fontWeight: "bold"}}>อาจารย์ที่ปรึกษา</Text>
+        <View style={styles.views2}></View>
     </View>
     </ScrollView>
   );
@@ -37,8 +46,10 @@ const styles = StyleSheet.create({
         alignItems: "center",
         borderWidth: 1,
         width: 330,
+        paddingBottom: 10,
         marginLeft: 30,
         marginTop: 30,
+        marginBottom:30,
         borderColor: "#AEAEAE",
         borderTopLeftRadius: 5,
         borderTopRightRadius: 5,
@@ -57,7 +68,7 @@ const styles = StyleSheet.create({
         marginLeft:170,
         marginTop: 10,
         justifyContent: "center",
-      },
+    },
       textInput: {
         width: 280,
         height: 80,
@@ -65,6 +76,34 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 8,
         marginTop: 5,
-      }
+    },
+    views: {
+        borderWidth: 1,
+        height: 80,
+        width: 275,
+        marginTop: 5,
+        borderColor: "#AEAEAE",
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+    },
+    views2: {
+        borderWidth: 1,
+        height: 50,
+        width: 275,
+        marginTop: 5,
+        borderColor: "#AEAEAE",
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+    },
+    expic: {
+        width: 275,
+        height: 150,
+        marginTop: 15,
+        borderRadius: 5,
+    }
 })
 export default PostMain;
