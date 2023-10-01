a3a60aedafcd9fb1dde3e596fc255bb6b0b3feee
Madara415 <88467996+madara1150@users.noreply.github.com>
2023-10-01 16:03:46 +0700

    feat: main page edit

---
+++ components/ProductCard.tsx
@@ -0,0 +1,62 @@
+import { Button, Card, Chip } from "@rneui/themed";
+import { View, Text } from "react-native";
+
+type ProductCardProps = {
+  image?: string;
+  name: string;
+  category: string;
+  owner: string;
+  advisor: string;
+};
+
+export const ProductCard = ({
+  image,
+  name,
+  category,
+  owner,
+  advisor,
+}: ProductCardProps) => {
+  return (
+    <Card containerStyle={{ borderRadius: 20, padding: 25 }}>
+      <View style={{ display: "flex", alignItems: "center" }}>
+        <Card.Image
+          style={{ width: 150, height: 150, borderRadius: 3 }}
+          source={{
+            uri: `${image}`,
+          }}
+        />
+      </View>
+
+      <Card.Title style={{ color: "#0098DA", fontSize: 24, marginTop: 5 }}>
+        {name}
+      </Card.Title>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 16 }}>ประเภทของโครงงาน : </Text>
+        <Chip title={category} type="outline" size="sm" />
+      </View>
+      <View style={{ display: "flex", flexDirection: "row", marginTop: 5 }}>
+        <Text style={{ fontSize: 16 }}>เจ้าของโครงงาน : </Text>
+        <Text style={{ fontSize: 16 }}>{owner}</Text>
+      </View>
+      <View style={{ display: "flex", flexDirection: "row", marginTop: 5 }}>
+        <Text style={{ fontSize: 16 }}>อาจารย์ที่ปรึกษา : </Text>
+        <Text style={{ fontSize: 16 }}>{advisor}</Text>
+      </View>
+
+      <Button
+        title={"แสดงรายละเอียด"}
+        buttonStyle={{
+          marginTop: 20,
+          borderRadius: 8,
+          backgroundColor: "#81ADC8",
+        }}
+      />
+    </Card>
+  );
+};
