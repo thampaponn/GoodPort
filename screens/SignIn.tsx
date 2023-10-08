1d6dad5017193dccda4866f26ddf9e9d2270575d
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-10-08 14:52:17 +0700

    backend

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -27,7 +27,7 @@ const SignIn = ({ navigation }) => {
     try {
       setLoading(true);
       const response = await axios.post(
-        "http://192.168.1.43:3000/auth/signin",
+        "http://192.168.1.42:3000/auth/signin",
         {
           username,
           password,
