349403e996bf3cd59cf41b478d17f84fcd0b16ae
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2023-10-18 15:24:15 +0700

    signUp api connected (#24)

---
+++ hook/typeOfRegisterScreen.ts
@@ -0,0 +1,19 @@
+export const typeOfRegisterScreen = () => {
+  return [
+    {
+      id: "1",
+      label: "อาจารย์ (@KMITL)",
+      value: "professor",
+    },
+    {
+      id: "2",
+      label: "นักเรียน (@KMITL)",
+      value: "student",
+    },
+    {
+      id: "3",
+      label: "ผู้เยี่ยมชม",
+      value: "visitor",
+    },
+  ];
+};
