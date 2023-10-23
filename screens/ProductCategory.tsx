034e3b01fcc576b5f5abbb1eb176a380309c2b5b
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-10-23 22:41:28 +0700

    update profilecard

--- screens/ProductCategory.tsx
+++ screens/ProductCategory.tsx
@@ -25,15 +25,15 @@ const ProductCategory = () => {
         </Card>
         <View style={{ margin: 15 }}>
           <View style={{ justifyContent: "center", borderRadius: 10, borderColor: "#D9D9D9", borderWidth: 1, padding: 10 }}>
-            <Text style={{ textAlign: "left", fontSize: 18, fontWeight: "600", color: "#0098DA" }}>ความพยายามอยู่ที่ไหน ความสำเร็จอยู่ที่นั่น</Text>
-            <Text style={{ textAlign: "left" }}>ประเภทของโครงงาน : Other</Text>
-            <Text style={{ textAlign: "left" }}>อาจารย์ที่ปรึกษา : อาจารย์ขิม</Text>
-            <View style={{ alignItems: "center" }}>
-              <TouchableOpacity style={{}}>
-                <Text>แสดงรายละเอียด</Text>
+            <Text style={{ textAlign: "left", fontSize: 18, fontWeight: "600", color: "#0098DA", marginLeft: 5 }}>ความพยายามอยู่ที่ไหน ความสำเร็จอยู่ที่นั่น</Text>
+            <Text style={{ textAlign: "left", marginLeft: 5 }}>ประเภทของโครงงาน : Other</Text>
+            <Text style={{ textAlign: "left", marginLeft: 5 }}>อาจารย์ที่ปรึกษา : อาจารย์ขิม</Text>
+            <View style={{ alignItems: "center", marginTop: 5 }}>
+              <TouchableOpacity style={{borderRadius: 5, borderColor: "#D9D9D9", borderWidth: 1, width: "100%", padding: 5, justifyContent: "center", margin: 5}}>
+                <Text style={{textAlign: "center", fontSize: 16, fontWeight: "600"}}>แสดงรายละเอียด</Text>
               </TouchableOpacity>
-              <TouchableOpacity>
-                <Text>ยืนยัน</Text>
+              <TouchableOpacity style={{borderRadius: 5, backgroundColor: "#86D789", width: "100%", padding: 5, justifyContent: "center", margin: 5}}>
+                <Text style={{textAlign: "center", fontSize: 16, fontWeight: "600", color: "#FFFFFF"}}>ยืนยัน</Text>
               </TouchableOpacity>
             </View>
           </View>
